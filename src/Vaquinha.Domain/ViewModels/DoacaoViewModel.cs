85e678c40f89e712bd8b16af3ac3e3846492345b
Marcelo Calixto <marccalixto@hotmail.com>
2020-12-07 20:21:30 -0400

    Commit inicial

---
+++ src/Vaquinha.Domain/ViewModels/DoacaoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Vaquinha.Domain.ViewModels
+{
+    public class DoacaoViewModel
+    {
+        public decimal Valor { get; set; }
+        public bool AceitaTaxa { get; set; }
+        public PessoaViewModel DadosPessoais { get; set; }
+        public EnderecoViewModel EnderecoCobranca { get; set; }
+        public CartaoCreditoViewModel FormaPagamento { get; set; }
+    }
+}
